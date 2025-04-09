f55597aeefc557ed91dc4c760669efbce2bc03b1
Merim <merim.hasanbegovic@outlook.com>
2025-04-09 18:47:41 +0200

    fix: router issue

--- routes/users.ts
+++ routes/users.ts
@@ -1,6 +1,5 @@
-import express from "express";
-const router = express.Router();
-
+import express from 'express'
+const router = express.Router()
 
 // const upload = require("../config/multer");
 
@@ -12,40 +11,27 @@ import {
   sendResetPasswordMail,
   resetPassword,
   verifyResetCode,
-  updateUser,
-  updatePassword,
-} from "../controllers/users";
-
-
-
-router.post("/register", register);
-
-router.post("/login", login);
-
-router.post("/forgot", sendResetPasswordMail);
-
-router.put("/verify/:code", verifyResetCode);
-
-router.put("/reset", resetPassword);
-
-router.put("/email", updateUser);
-
-router.put(
-  "/password",
-  updatePassword
-);
-
-router.get("/user", getUser);
-
-router.put(
-  "/user",
   updateUser
-);
+} from '../controllers/users'
+
+router.post('/register', register)
+
+router.post('/login', login)
+
+router.route('/user').get(getUser).put(updateUser)
+
+router.post('/forgot', sendResetPasswordMail)
+
+router.put('/verify/:code', verifyResetCode)
+
+router.put('/reset', resetPassword)
+
+router.put('/email', updateUser)
 
 router.put(
-  "/avatar",
+  '/avatar',
   // upload.single("avatar"),
   changeAvatar
-);
+)
 
-export default router;
+export default router
